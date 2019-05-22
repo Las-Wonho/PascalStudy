348cad3d837a31c6b1f30be402bb7bd6be6ffaec
Las-Wonho <haho013039@gmail.com>
2019-05-22 05:46:02 +0000

    My First Pascal Program - 10561

--- first_pascal.pas
+++ first_pascal.pas
@@ -38,6 +38,32 @@ begin
 end;
 
 
+function count(idx: i8, pr: i8, s1: i8, s2: i8,len: i8, n: String): i8;
+var
+   res, tmp: i8;
+begin
+   res := 0
+   if idx == len then
+      res := s1;
+   else
+      if s1 > 0 then
+         for i := 0 to 10 do begin
+            if s2 == 0 then
+               if pr <= i then
+                  res := res + count(idx + 1, i, s1, 0, n);
+               else
+                  res := res + count(idx + 1, i, s1, 1, n);
+            else
+               if pr >= i then
+                  res := res + count(idx + 1, i, s1, 1, n);
+         end;
+      else
+        for i := 0 to n[idx] do begin
+        
+        end;
+end
+
+
 var
    n:String;
    arr_len:i8;
